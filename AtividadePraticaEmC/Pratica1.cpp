baa6d17da73c8436e0bc4c48930aa5fc2294dbfe
Jose Eduardo Silva Dias <J.eduardodias@outlook.com>
2025-07-06 18:54:25 -0300

    Update Pratica1.cpp

--- AtividadePraticaEmC/Pratica1.cpp
+++ AtividadePraticaEmC/Pratica1.cpp
@@ -12,10 +12,10 @@ struct Notas {
 int main() {
     setlocale(LC_ALL, "Portuguese"); // serve paraaceitar os acentos da lingua portuguesa
 
-    struct Notas notas; // struct para guardar as notas
+    struct Notas notas; // strut para guardar as notas
     float media;
 
-    // pedir as notas uma por uma entre 0 e 100
+    // pede as notas uma por uma entre 0 e 100
 
     do {
         printf("Digite a nota da APOL1 (0 a 100): ");
@@ -23,13 +23,13 @@ int main() {
     } while (notas.notaApol1 < 0 || notas.notaApol1 > 100);
 
     do {
-        printf("Digite a nota da APOL2 (0 a 100): ");
+        printf("Digte a nota da APOL2 (0 a 100): ");
         scanf_s("%d", &notas.notaApol2);
     } while (notas.notaApol2 < 0 || notas.notaApol2 > 100);
 
     do {
         printf("Digite a nota da Prova Objetiva (0 a 100): ");
-        scanf_s("%d", &notas.notaProva );
+        scanf_s("%d", &notas.notaProva);
     } while (notas.notaProva < 0 || notas.notaProva > 100);
 
     do {
@@ -37,16 +37,16 @@ int main() {
         scanf_s("%d", &notas.notaAtividade);
     } while (notas.notaAtividade < 0 || notas.notaAtividade > 100);
 
-    // calcula  a media
+    // calucla  a media
     media = notas.notaApol1 * 0.15 + notas.notaApol2 * 0.15 +
         notas.notaProva * 0.30 + notas.notaAtividade * 0.40;
 
-    
+
     printf("Sua média final foi: %.2f\n", media); // Mostra a média
 
 
     // ve se o aluno passou, ficou de exame ou reprovou
-    if ( media >= 70) {
+    if (media >= 70) {
         printf("FOI APROVADO!!\n");
     }
     else if (media >= 30) {
